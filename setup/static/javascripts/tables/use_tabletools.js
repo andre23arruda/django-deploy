1182f7b006938309c40f7794c667b5ffe5ac0231
andre23arruda <andre23arruda@gmail.com>
2020-07-17 16:49:08 -0300

    feat: add button widget dasboard and improve pacientes table

--- setup/static/javascripts/tables/use_tabletools.js
+++ setup/static/javascripts/tables/use_tabletools.js
@@ -1,5 +1,3 @@
-
-
 (function( $ ) {
 
 	'use strict';
@@ -8,22 +6,9 @@
 		var $table = $('#datatable-tabletools');
 
 		$table.dataTable({
-			sDom: "<'text-left mb-md'T>" + $.fn.dataTable.defaults.sDom,
+			sDom: $.fn.dataTable.defaults.sDom + "<'text-right mt-lg'T>",
 			oTableTools: {
-				sSwfPath: $table.data('swf-path'),
 				aButtons: [
-					{
-						sExtends: 'pdf',
-						sButtonText: 'PDF'
-					},
-					{
-						sExtends: 'csv',
-						sButtonText: 'CSV'
-					},
-					{
-						sExtends: 'xls',
-						sButtonText: 'Excel'
-					},
 					{
 						sExtends: 'print',
 						sButtonText: 'Imprimir',
